150d1bc70c554814e6cabf206ce5db2764621348
robert roszak <robert.roszak@outlook.com>
2019-07-17 14:46:22 +0200

    Add project files.

---
+++ RiskyPlaceTracker/Controllers/PlaceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RiskyPlaceTracker.Controllers
+{
+    public class PlaceController : Controller
+    {
+        // GET: Place
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //POST
+        public ActionResult AddPlace()
+        {
+            return View();
+        }
+    }
+}
---
+++ RiskyPlaceTracker/Models/Place.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RiskyPlaceTracker.Models
+{
+    public class Place
+    {
+        [ScaffoldColumn(false)]
+        public int PlaceId { get; set; }
+        [Display(Name="Nazwa")]
+        public string Name { get; set; }
+        [Display(Name="Opis")]
+        public string Description { get; set; }
+        [ScaffoldColumn(false)]
+        public byte[] Photo { get; set; }
+        [ScaffoldColumn(false)]
+        public int AddedBy { get; set; }
+        [ScaffoldColumn(false)]
+        public DateTime AddedOn { get; set; }
+    }
+}
